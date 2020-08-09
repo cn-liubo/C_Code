54087447794ebfe324c1894fcc1105f87d53e6a1
‘special1024’ <394585940@qq.com>
2020-08-09 15:13:11 +0800

    提交代码

---
+++ test_8.7.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//int max(int x, int y)
+//{
+//
+//	return x > y ? x : y;
+//
+//}
+//int main()
+//{
+//	int val = max(23, 45);
+//	printf("%d\n", val);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    char arr[] = { 'b', 'i', 't' };
+//    printf("%d\n", strlen(arr));
+//    return 0;
+//}
+
+
+////转义字符
+//int main()
+//{
+//	printf("c:\code\test.c");
+//}
+
+//int main()
+//{
+//	char arr1[] = "bit";//后面有一个隐藏的'\0'
+//	char arr2[] = { 'b','i','t' };
+//	//'\0'字符串的结束标志
+//	//sizeof()计算的是所占空间的大小，和'\0'无关
+//	//strlen()只能求字符串长度，和'\0'有关,需要以'\0'为结束标志
+//	printf("%d\n", sizeof(arr1));//4
+//	printf("%d\n", sizeof(arr2));//3
+//	printf("%d\n", arr1[0]);
+//	printf("%d\n", arr1[1]);
+//	printf("%d\n", arr1[2]);
+//	printf("%d\n", arr2[0]);
+//	printf("%d\n", arr2[1]);
+//	printf("%d\n", arr2[2]);
+//
+//
+//	//char arr1[] = "bit";
+//	//char arr2[] = { 'b','i','t' };
+//	//printf("%d\n", strlen(arr1));//3
+//	//printf("%d\n", strlen(arr2));//15随机值
+//
+//}
+
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = {'a','b','c','d','e','f','\0'};
+//
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//}
+
+
+
+//#define NUM 100
+//int main()
+//{
+//	int i[NUM] = { 1,2,3,4 };
+//
+//	return 0;
+//}
+
+
+//int num = 10;
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	printf("%d", num);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//
+//
+//	/*int num = 0;
+//	scanf("%d", &num);
+//	printf("%d",num);
+//	return 0;*/
+//}
