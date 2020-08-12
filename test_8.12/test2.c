851b4817d983b65e8fdfd2c29e7c220d8eedce6c
394585940@qq.com <Liu19991225>
2020-08-12 15:13:23 +0800

    提交代码

---
+++ test_8.12/test2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main() {
+	printf("%d\n", 7 / 2);//3
+	printf("%f\n", 7 / 2.0);//3.5
+}
+
+//int main()
+//{
+//	int arr[10] = { 0,1,2,3,4,5 };
+//	for (int i = 0; i < 10; i++) {
+//		printf("%d\n",arr[i]);
+//	}
+//}
